228091b670cd31543d1a7aec8af6bdb5d31d2d5d
skerdikasa7-prog <skerdikasa7@gmail.com>
2025-08-26 02:06:06 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return (
+    <div className="p-10 text-center text-2xl font-bold text-lime-400">
+      🚀 Nexora Online! Tutto caricato correttamente 💯
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
