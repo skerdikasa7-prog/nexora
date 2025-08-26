c9fdf8fb073af4e051bb130feb3b124913a87760
skerdikasa7-prog <skerdikasa7@gmail.com>
2025-08-26 03:07:29 +0200

    Delete badge.jsx

--- badge.jsx
+++
@@ -1,6 +0,0 @@
-
-import React from 'react'
-export const Badge = ({className='', children, variant='secondary'}) => {
-  const styles = variant==='secondary' ? 'bg-zinc-800 text-zinc-200' : 'bg-lime-500 text-black'
-  return <span className={`px-2 py-1 rounded-full text-xs ${styles} ${className}`}>{children}</span>
-}
