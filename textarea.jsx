de74b5d8d436aaeedeb0bb5a2b16c534fe71bb81
skerdikasa7-prog <skerdikasa7@gmail.com>
2025-08-26 03:05:10 +0200

    Add files via upload

---
+++ textarea.jsx
@@ -0,0 +1,3 @@
+
+import React from 'react'
+export const Textarea = ({className='', ...props}) => <textarea className={`rounded-xl bg-zinc-800 border border-zinc-700 px-3 py-2 text-sm w-full ${className}`} {...props} />
