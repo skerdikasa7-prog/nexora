8be78628935aad00c78cff51fca05a145795beac
skerdikasa7-prog <skerdikasa7@gmail.com>
2025-08-26 03:07:56 +0200

    Delete card.jsx

--- card.jsx
+++
@@ -1,6 +0,0 @@
-
-import React from 'react'
-export const Card = ({className='', children}) => <div className={`rounded-2xl border border-zinc-800 bg-zinc-900/50 ${className}`}>{children}</div>
-export const CardHeader = ({className='', children}) => <div className={`p-4 border-b border-zinc-800 ${className}`}>{children}</div>
-export const CardContent = ({className='', children}) => <div className={`p-4 ${className}`}>{children}</div>
-export const CardFooter = ({className='', children}) => <div className={`p-4 pt-0 ${className}`}>{children}</div>
